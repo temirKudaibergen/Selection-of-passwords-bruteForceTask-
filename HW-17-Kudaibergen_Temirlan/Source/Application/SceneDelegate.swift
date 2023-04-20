9d2905cf759fba18b37d4175f6a05380bd705d6f
temirlan <k.temirlan.t@gmail.com>
2023-04-20 12:49:13 +0600

    подготовка проекта

---
+++ HW-17-Kudaibergen_Temirlan/Source/Application/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  HW-17-Kudaibergen_Temirlan
+//
+//  Created by Темирлан Кудайберген on 20.04.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
